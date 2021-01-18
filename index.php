aa5b1930621244366b021977217c586398bf02d1
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-01-18 11:06:36 -0300

    versionando com turma 13b do itau o primeiro projeto deploy

---
+++ index.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<body style="background-color:#6495ED;text-align:center;padding-top: 42px;font-size: 20px;font-family: arial;">
+  Uma aula com a turma 13b do itaú
+  <br>
+  Mais uma mudança
+</body>
+</html>
