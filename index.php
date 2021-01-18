056a638224daed4535a6b6e28a025e3d0becb3ae
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2021-01-18 11:07:24 -0300

    mudando de propósito

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <body style="background-color:#6495ED;text-align:center;padding-top: 42px;font-size: 20px;font-family: arial;">
-  Uma aula com a turma 13b do itaú
+  <h1>Uma aula com a turma 13b do itaú</h1>
   <br>
   Mais uma mudança
 </body>
